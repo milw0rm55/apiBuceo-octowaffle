77e42dc2960abd7566bd62ea2e74304497d201f6
milw0rm55 <sebastiantace@gmail.com>
2019-06-15 19:31:53 +0200

    pruebafg

--- Alumnos/read_one.php
+++ Alumnos/read_one.php
@@ -20,34 +20,48 @@ $alumno = new Alumnos($db);
 // set ID property of record to read
 $alumno->ID_Alumno = isset($_GET['ID_Alumno']) ? $_GET['ID_Alumno'] : die();
 $alumno->readOne();
-echo var_dump($alumno);
+$num = $alumno->rowCount();
 // read the details of alumno to be edited
-if($alumno->Nombre!=null){
-    // create array
-    $alumno_arr = array(
-        "ID_Alumno" => $ID_Alumno,
-        "ID_Usuario" => $ID_Usuario,
-        "Nombre" => $Nombre,
-        "Apellidos" => $Apellidos,
-        "DNI" => $DNI,
-        "Titulacion" => $Titulacion,
-        "Telefono" => $Telefono,
-        "Curso" => $Curso,
+if($num>0){
 
-    );
+    // alumnoss array
+    $alumnos_arr=array();
+    $alumnos_arr["records"]=array();
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+        $alumnos_item=array(
+            "ID_Alumno" => $ID_Alumno,
+            "ID_Usuario" => $ID_Usuario,
+            "Nombre" => $Nombre,
+            "Apellidos" => $Apellidos,
+            "DNI" => $DNI,
+            "Titulacion" => $Titulacion,
+            "Telefono" => $Telefono,
 
+        );
+
+        array_push($alumnos_arr["records"], $alumnos_item);
+    }
     // set response code - 200 OK
     http_response_code(200);
 
-    // make it json format
-    echo json_encode($alumno_arr);
+    // show alumnoss data in json format
+    echo json_encode($alumnos_arr);
 }
 
 else{
+
     // set response code - 404 Not found
     http_response_code(404);
 
-    // tell the user $alumno does not exist
-    echo json_encode(array("message" => "Alumno does not exist."));
+    // tell the user no alumnos found
+    echo json_encode(
+        array("message" => "No alumnos found.")
+    );
 }
-?>
