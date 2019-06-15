2a438abaef845651f31e959ff3c3581188dfec28
milw0rm55 <sebastiantace@gmail.com>
2019-06-15 20:08:34 +0200

    prueba definitiva 5

--- Alumnos/read_one.php
+++ Alumnos/read_one.php
@@ -22,7 +22,7 @@ $alumno->ID_Alumno = isset($_GET['ID_Alumno']) ? $_GET['ID_Alumno'] : die();
 
 // read the details of alumno to be edited
 $alumno->readOne();
-echo "AAAAAAAAAA".var_dump($alumno->Nombre);
+
 if($alumno->Nombre!=null){
     // create array
     $alumno_arr = array(
