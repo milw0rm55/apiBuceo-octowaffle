4066cd290f35481b02222722695b0b3c37a872d3
milw0rm55 <sebastiantace@gmail.com>
2019-06-15 13:19:08 +0200

    prueba 35

--- Alumnos/read_one.php
+++ Alumnos/read_one.php
@@ -19,7 +19,9 @@ $alumno = new Alumnos($db);
 
 // set ID property of record to read
 $alumno->ID_Alumno = isset($_GET['ID_Alumno']) ? $_GET['ID_Alumno'] : die();
+$alumno->readOne();
 echo $alumno->ID_Alumno;
+echo $alumno ->Nombre;
 // read the details of alumno to be edited
 if($alumno->Nombre!=null){
     // create array
