46f8e1c5195f7ceca8d377072ba9137d8bba8c52
milw0rm55 <sebastiantace@gmail.com>
2019-06-15 19:33:29 +0200

    pruebadg89yh

--- objects/Alumnos.php
+++ objects/Alumnos.php
@@ -82,6 +82,7 @@ class Alumnos
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
+        echo var_dump($stmt);
         $stmt->bindParam(1, $this->ID_Alumno);
         // execute query
         $stmt->execute();
