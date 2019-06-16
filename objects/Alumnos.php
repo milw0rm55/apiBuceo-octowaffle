3b31cd709e3ca2066ad449a6ea22a8e4523c9e0e
milw0rm55 <sebastiantace@gmail.com>
2019-06-16 09:54:53 +0200

    prueba echo 13

--- objects/Alumnos.php
+++ objects/Alumnos.php
@@ -126,8 +126,6 @@ class Alumnos
         $this->Telefono = htmlspecialchars(strip_tags($this->Telefono));
         $this->ID_Alumno = htmlspecialchars(strip_tags($this->ID_Alumno));
 
-        echo $this->Apellido;
-        echo $this->ID_Usuario;
         // bind new values
         $stmt->bindParam(':Nombre', $this->Nombre);
         $stmt->bindParam(':Apellido', $this->Apellido);
@@ -137,6 +135,7 @@ class Alumnos
         $stmt->bindParam(':ID_Alumno', $this->ID_Alumno);
 
         // execute the query
+        echo var_dump($stmt);
         if ($stmt->execute()) {
             return true;
         }
